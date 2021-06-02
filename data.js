c41d9fd30506e1fa1a33bf6183a544d5266f642b
rafaelarango <arangoramirezrafael@gmail.com>
2021-06-02 11:33:45 -0500

    simulamos una base de datos creando un archivo con la data

---
+++ data.js
@@ -0,0 +1,22 @@
+const libros = [
+
+        {
+            titulo: "Cronicas de juventu",
+            autor: "Guy Delisle",
+            precio: "15,20 euros"
+        },
+
+        {
+            titulo: "Cronicas de juventu",
+            autor: "Guy Delisle",
+            precio: "15,20 euros"
+        },
+
+        {
+            titulo: "Sobre el duelo",
+            autor: "Chimamanda",
+            precio: "6,55 euros"
+        },
+
+
+]
